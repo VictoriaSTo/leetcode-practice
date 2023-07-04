2aa15d85742d4e70eed9f1d0ebbec52103a5a459
VictoriaSto <v.stoichuk@gmail.com>
2023-07-04 14:21:39 -0400

    Convert Binary Number in a Linked List to Integer

--- 2515.js
+++ 2515.js
@@ -19,3 +19,21 @@ var closetTarget = function(words, target, startIndex) {
     }
     return -1;
 };
+
+// var closetTarget = function(words, target, startIndex) {
+//     let left = startIndex;
+//     let right = startIndex;
+//     let step = 0;
+//     let n = words.length;
+
+//     while(step <= n) {
+//         if(words[left] === target || words[right] === target) {
+//             return step;
+//         } else {
+//             right = (right + 1) % n;
+//             left = (left - 1 + n) % n;
+//         }
+//         step++;
+//     }
+//     return -1;
+// };
